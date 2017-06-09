76a38011f392732026f7a3d4ec5586e1df767b9b
Ayush Sharma <ayushstarksharma@gmail.com>
2017-06-10 00:30:46 +0530

    FLOW001

---
+++ FLOW001.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+ int t;
+ int a,b,sol;
+ int i;
+ scanf("%d",&t);
+  for(i=0;i<t;i++){
+        scanf("%d%d",&a,&b);
+                sol=a+b;
+                printf("%d\n",sol);
+                }
+
+
+
+
+
+
+return 0;}
